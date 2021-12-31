45d54844e6517e7caaf5ccd9aa192bccd59b9fcb
Moreno-Sanna <96881097+Moreno-Sanna@users.noreply.github.com>
2021-12-31 16:42:01 +0100

    Add files via upload

---
+++ 5_Count Retweet .R
@@ -0,0 +1,19 @@
+library(stringr)
+
+##Calcolo Retweet
+#Load Retweets matrix out of the file 4_Union Tweets_extraction datasets.R  
+load(file="R/TweetBMWRT.R")
+
+#Create a key for count the retweets shared every day, concatenating date of creation and original tweet id 
+
+Key <- paste (substr(TweetRT[,"created_at"],1,10),TweetRT[,"referenced_tweets.id_retweeted"], sep=" - ")
+
+Cont.Ret <- data.frame(table(Key))
+Cont.Ret <- cbind(data=substr(Cont.Ret[,1],1,10),id=substr(Cont.Ret[,1],14,999),Cont.Ret)
+
+#save the matrix
+save(Cont.Ret, file="####")
+
+
+
+
