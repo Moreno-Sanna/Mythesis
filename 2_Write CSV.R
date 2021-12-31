714ab1b693b8ade49b365ae5274aa296b3af3632
Moreno-Sanna <96881097+Moreno-Sanna@users.noreply.github.com>
2021-12-31 16:22:31 +0100

    Update 2_Write CSV.R

--- 2_Write CSV.R
+++ 2_Write CSV.R
@@ -5,6 +5,7 @@ load("####")
 
 Tweets<-Tweet
 head(Tweets)
+#Filter only the tweets not retweets
 indexRT<- ifelse(is.na(Tweets[,"referenced_tweets.type_retweeted"])==T,F,T)
 indexNOTRT<- indexRT==FALSE
 
@@ -14,7 +15,7 @@ Tweets1<- Tweets1[1:lim,]
 Tweets2 <- Tweets[indexNOTRT,]
 Tweets2<- Tweets2[(lim+1):nrow(Tweets[indexNOTRT,]),]
 
-#Every matrix is divided i two equal-size datasets
+#Every matrix is divided in two equal-size datasets
 write_excel_csv2(data.frame(Tweets1[,c(1,2,4)]),
                  file=paste("####",
                             gsub(x=substr(Tweets1[1,4],1,10),"-"," ")
